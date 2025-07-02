14a00de8c93c81c1ee4e687969524c9463c0510d
tthau2004@gmail.com <tthau2004@gmail.com>
2025-07-02 16:31:05 +0700

    Create website

---
+++ SaleApp/src/main/java/com/tthau/controllers/HomeController.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tthau.controllers;
+
+import com.tthau.pojo.Category;
+import com.tthau.services.CategoryService;
+import com.tthau.services.ProductService;
+import jakarta.persistence.Query;
+import java.util.Map;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ *
+ * @author admin
+ */
+@Controller
+public class HomeController {
+
+    @Autowired
+    private CategoryService cateService;
+    
+    @Autowired
+    private ProductService prodService;
+
+    @RequestMapping("/")
+    @Transactional
+    public String index(Model model, @RequestParam Map<String, String> params) {
+        model.addAttribute("categories", this.cateService.getCates());
+        model.addAttribute("products", this.prodService.getProducts(params));
+
+        return "index";
+    }
+}
---
+++ SaleApp/src/main/java/com/tthau/repositories/impl/ProductRepositoryImpl.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tthau.repositories.impl;
+
+import com.tthau.pojo.Product;
+import com.tthau.repositories.ProductRepository;
+import jakarta.persistence.Query;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author admin
+ */
+@Repository
+@Transactional
+public class ProductRepositoryImpl implements ProductRepository{
+
+    @Autowired
+    private LocalSessionFactoryBean factory;
+
+    private static final int PAGE_SIZE = 6;
+
+    @Override
+    public List<Product> getProducts(Map<String, String> params) {
+        Session s = this.factory.getObject().getCurrentSession();
+        CriteriaBuilder b = s.getCriteriaBuilder();
+        CriteriaQuery<Product> q = b.createQuery(Product.class);
+        Root root = q.from(Product.class);
+        q.select(root);
+
+        // Lọc dữ liệu
+        if (params != null) {
+            List<Predicate> predicates = new ArrayList<>();
+
+            String kw = params.get("kw");
+            if (!kw.isEmpty()) {
+                predicates.add(b.like(root.get("name"), String.format("%%%s%%", kw)));
+            }
+
+            String fromPrice = params.get("fromPrice");
+            if (fromPrice != null && !fromPrice.isEmpty()) {
+                predicates.add(b.greaterThanOrEqualTo(root.get("price"), fromPrice));
+            }
+
+            q.where(predicates.toArray(Predicate[]::new));
+        }
+
+        // Sắp xếp dữ liệu
+        Query query = s.createQuery(q);
+
+        // Phân trang
+        if (params != null) {
+            String page = params.get("page");
+            if (page != null) {
+                int p = Integer.parseInt(page);
+                int start = (p - 1) * PAGE_SIZE;
+
+                query.setFirstResult(start);
+                query.setMaxResults(PAGE_SIZE);
+            }
+        }
+
+        return query.getResultList();
+    }
+}
